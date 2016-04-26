4c1bea3ddcf32d7cbc6456c356d0c168f7542cba
Adam Pyle <admpyle@gmail.com>
2016-04-27 06:56:16 +1000

    Fixed symlink paths for script. > Removed destination directory and mapped the symlink properly Also fixed IF statement brackets > these were incorrectly laid out

--- start.sh
+++ start.sh
@@ -7,18 +7,19 @@
 # Items in /proxy/confg/conf.d/ get symlinked to the /etc/nginx/conf.d/ directory
 
 
-extraConfigDir="/proxy/confg/conf.d/"
-nginxConfD="/etc/nginx/conf.d/"
+extraConfigDir="/proxy/config/conf.d/"
+nginxConfD="/etc/nginx/conf.d"
 # Check if extraConfigDir has configurations and then link the folder
-if [ "$(ls -A $extraConfigDir)"]; then
+if [ "$(ls -A $extraConfigDir)" ]; then
   echo "Linking your configuration files"
+  rm -rf /etc/nginx/conf.d
   ln -s $extraConfigDir $nginxConfD
 else
   echo "No extra config, default confiuration is set"
 fi
 
 # Check if the nginx.conf file exists and link it
-if [ "$(ls -A /proxy/config/nginx.conf)"]; then
+if [ "$(ls -A /proxy/config/nginx.conf)" ]; then
   echo "Adding your nginx.conf file"
   rm /etc/nginx/nginx.conf
   ln -s /proxy/config/nginx.conf /etc/nginx/nginx.conf
